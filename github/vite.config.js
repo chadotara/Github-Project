63aa87e7cb509c11da49c9b3cc0e324a1fa20a8d
sunny chadotara <chadotara.sunny27@gmail.com>
2025-03-23 16:44:58 +0530

    chnages in version

--- github/vite.config.js
+++ github/vite.config.js
@@ -1,6 +1,18 @@
+// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      // Ensure that the same instance of @emotion/react is used throughout.
+      '@emotion/react': path.resolve(__dirname, 'node_modules/@emotion/react'),
+      '@emotion/styled': path.resolve(__dirname, 'node_modules/@emotion/styled')
+    }
+  },
+  optimizeDeps: {
+    include: ['@emotion/react']
+  }
 });
